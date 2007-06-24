e0d70e21054648d6a4abdb62695b80bccda88453
andykim78 <andykim78@25f2d1f3-9e33-0410-bf15-d1b47774bbb0>
2007-06-24 07:15:36 +0000

    - Searching by order number possible now - Cleaned up sql code for the admin dashboard - Applied patch from Jamie Hardt -- The admin page no longer craps out when there are no orders.
    git-svn-id: https://potionstore.googlecode.com/svn/trunk@4 25f2d1f3-9e33-0410-bf15-d1b47774bbb0

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,3 @@
-
 class AdminController < ApplicationController
   # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
   verify :method => :post, :only => [ :destroy, :create, :update ],
@@ -23,7 +22,7 @@ class AdminController < ApplicationController
       session[:logged_in] = true
       redirect_to :action => session[:intended_action]
     else
-      flash[:notice] = 'Go home kid. This ain\'t for you.'
+      flash[:notice] = "Go home kid. This ain't for you."
       render :action => "signin_form", :layout => 'error'
     end
   end
@@ -33,28 +32,58 @@ class AdminController < ApplicationController
     redirect_to home_url
   end
 
-  # Welcome page
+  # Dashboard page
   def index
-    @reports = {}
-
-    # NOTE: mysql should use IFNULL instead of COALESCE
     
-    @reports['Today'] = Order.find_by_sql("select (select count(*) from orders where status = 'C' and payment_type != 'free' and date(order_time) = current_date) as orders, sum(line_items.unit_price * quantity) - sum(coalesce(coupons.amount, 0)) - sum(line_items.unit_price * quantity * coalesce(percentage, 0) / 100) as earned, sum(quantity) as q, products.name as product from orders inner join line_items on orders.id = line_items.order_id left outer join products on products.id = line_items.product_id left outer join coupons on coupons.id = orders.coupon_id where status = 'C' and payment_type != 'free' and date(order_time) = current_date group by product")
-	@reports['Last 7 Days'] = Order.find_by_sql("select (select count(*) from orders where status = 'C' and payment_type != 'free' and current_date - 7 <= order_time) as orders, sum(line_items.unit_price * quantity) - sum(coalesce(coupons.amount, 0)) - sum(line_items.unit_price * quantity * coalesce(percentage, 0) / 100) as earned, sum(quantity) as q, products.name as product from orders inner join line_items on orders.id = line_items.order_id left outer join products on products.id = line_items.product_id left outer join coupons on coupons.id = orders.coupon_id where status = 'C' and payment_type != 'free' and current_date - 7 <= order_time group by product")
-    @reports['Last 30 Days'] = Order.find_by_sql("select (select count(*) from orders where status = 'C' and payment_type != 'free' and current_date - 30 <= order_time) as orders, sum(line_items.unit_price * quantity) - sum(coalesce(coupons.amount, 0)) - sum(line_items.unit_price * quantity * coalesce(percentage, 0) / 100) as earned, sum(quantity) as q, products.name as product from orders inner join line_items on orders.id = line_items.order_id left outer join products on products.id = line_items.product_id left outer join coupons on coupons.id = orders.coupon_id where status = 'C' and payment_type != 'free' and current_date - 30 <= order_time group by product")
-    @reports['Last 365 Days'] = Order.find_by_sql("select (select count(*) from orders where status = 'C' and payment_type != 'free' and current_date - 365 <= order_time) as orders, sum(line_items.unit_price * quantity) - sum(coalesce(coupons.amount, 0)) - sum(line_items.unit_price * quantity * coalesce(percentage, 0) / 100) as earned, sum(quantity) as q, products.name as product from orders inner join line_items on orders.id = line_items.order_id left outer join products on products.id = line_items.product_id left outer join coupons on coupons.id = orders.coupon_id where status = 'C' and payment_type != 'free' and current_date - 365 <= order_time group by product")
+    @reports = {}
+    
+    if Product.count == 0
+      flash[:notice] = "This store doesn't have any products! Add some!"
+      redirect_to :action => 'products' and return
+    end
 
+    # NOTE: We have to use SQL because performance is completely unacceptable otherwise
+
+    # helper function
+    def last_n_days_sql(days)
+      # NOTE: mysql should use IFNULL instead of COALESCE
+      return "
+        select (select count(*)
+                  from orders
+                 where status = 'C' and
+                       payment_type != 'free' and
+                       current_date - #{days} <= order_time) as orders,
+               sum(line_items.unit_price * quantity)
+                 - sum(coalesce(coupons.amount, 0))
+                 - sum(line_items.unit_price * quantity * coalesce(percentage, 0) / 100) as earned,
+               sum(quantity) as q,
+               products.name as product
+
+        from orders
+             inner join line_items on orders.id = line_items.order_id
+             left outer join products on products.id = line_items.product_id
+             left outer join coupons on coupons.id = orders.coupon_id
+
+        where status = 'C' and lower(payment_type) != 'free' and current_date - #{days} <= order_time
+
+        group by product"
+    end
+
+    @reports['Today'] = Order.find_by_sql(last_n_days_sql(1))
+    @reports['Last 7 Days'] = Order.find_by_sql(last_n_days_sql(7))
+    @reports['Last 30 Days'] = Order.find_by_sql(last_n_days_sql(30))
+    @reports['Last 365 Days'] = Order.find_by_sql(last_n_days_sql(365))
 
     @report_num_orders = {}
     @report_totals = {}
-    
+  
     for key in ['Today', 'Last 7 Days', 'Last 30 Days', 'Last 365 Days']
       report = @reports[key]
       orders = 0
       total = 0
       for product in report
         orders = product.orders
-        total += product.earned.to_f
+        total += product.earned if product
       end
       @report_num_orders[key] = orders
       @report_totals[key] = total
@@ -63,48 +92,54 @@ class AdminController < ApplicationController
     # NOTE: mysql uses year, month, and day functions instead of date_part
 
     last_8_days_sql = "
-		select date_part('year', orders.order_time) as year,
-			   date_part('month', orders.order_time) as month,
-			   date_part('day', orders.order_time) as day,
-			   sum(line_items.unit_price * quantity) - sum(COALESCE(coupons.amount, 0))
-				- sum(line_items.unit_price * quantity * COALESCE(percentage, 0) / 100) as earned,
-			   max(orders.order_time) as last_time
+        select date_part('year', orders.order_time) as year,
+               date_part('month', orders.order_time) as month,
+               date_part('day', orders.order_time) as day,
+               sum(line_items.unit_price * quantity) - sum(coalesce(coupons.amount, 0))
+                  - sum(line_items.unit_price * quantity * coalesce(percentage, 0) / 100) as earned,
+               max(orders.order_time) as last_time
 
-		from orders 
-			 inner join line_items on orders.id = line_items.order_id 
-			 left outer join coupons on coupons.id = orders.coupon_id 
+        from orders 
+             inner join line_items on orders.id = line_items.order_id 
+             left outer join coupons on coupons.id = orders.coupon_id 
 
-		where status = 'C' and payment_type != 'Free'
+        where status = 'C' and payment_type != 'Free'
 
-		group by year, month, day
+        group by year, month, day
 
-		order by last_time desc limit 8"
+        order by last_time desc limit 8"
 
     last_8_months_sql = "
-		select date_part('year', orders.order_time) as year,
-			   date_part('month', orders.order_time) as month,
-			   sum(line_items.unit_price * quantity) - sum(COALESCE(coupons.amount, 0))
-				- sum(line_items.unit_price * quantity * COALESCE(percentage, 0) / 100) as earned,
-			   max(orders.order_time) as last_time
+        select date_part('year', orders.order_time) as year,
+               date_part('month', orders.order_time) as month,
+               sum(line_items.unit_price * quantity) - sum(coalesce(coupons.amount, 0))
+                  - sum(line_items.unit_price * quantity * coalesce(percentage, 0) / 100) as earned,
+               max(orders.order_time) as last_time
 
-		from orders
-			 inner join line_items on orders.id = line_items.order_id
-			 left outer join coupons on coupons.id = orders.coupon_id
+        from orders
+             inner join line_items on orders.id = line_items.order_id
+             left outer join coupons on coupons.id = orders.coupon_id
 
-		where status = 'C' and payment_type != 'Free'
+        where status = 'C' and payment_type != 'Free'
 
-		group by year, month
+        group by year, month
 
-		order by last_time desc limit 8"
+        order by last_time desc limit 8"
 
-  	@daily = Order.find_by_sql(last_8_days_sql)
+    @daily = Order.find_by_sql(last_8_days_sql)
     @monthly = Order.find_by_sql(last_8_months_sql)
 
+    # Calculate a very simple estimate.
+    # Takes the average daily sales from the last 30 days to extrapolate the sales
+    # for the remaining days of the current month
     today = Date.today
-    month_days = Date.civil(today.year, today.month, -1).day
-    
-    @month_estimate = @monthly.first.earned.to_f + @report_totals['Last 30 Days'] / 30 * (month_days - today.day)
-    @year_estimate = @month_estimate * 12
+    days_in_current_month = Date.civil(today.year, today.month, -1).day
+
+    if @monthly != nil and @monthly.length > 0
+      @month_estimate = @monthly.first.earned.to_f + @report_totals['Last 30 Days'] / 30 * (days_in_current_month - today.day)
+      @year_estimate = @month_estimate * 12
+    end
+
   end
 
   # Order actions
@@ -113,9 +148,10 @@ class AdminController < ApplicationController
     q.strip!
     redirect_to :back and return if not q
     conditions = "status != 'P' AND (email ~* '#{q}' OR
-									 first_name ~* '#{q}.*' OR
-									 last_name ~* '#{q}.*' OR
-									 licensee_name ~* '%#{q}.*')"
+                                     first_name ~* '#{q}.*' OR
+                                     last_name ~* '#{q}.*' OR
+                                     licensee_name ~* '%#{q}.*' OR
+                                     id = #{q})"
     @order_pages, @orders = paginate :orders, :per_page => 100, :order => 'order_time DESC', :conditions => conditions
     render :action => "orders"
   end
@@ -190,20 +226,23 @@ class AdminController < ApplicationController
     redirect_to :action => 'order', :id => @order.id
   end
 
+
   # Product actions
-  def products
+  # Modifying a product doesn't happen often enough, so I am doing it directly through the
+  # database for now.
+  def products # unused
     @product_pages, @products = paginate :products, :per_page => 10
   end
 
-  def product
+  def product # unused
     @product = Product.find(params[:id])
   end
 
-  def new_product
+  def new_product # unused
     @product = Product.new
   end
 
-  def create_product
+  def create_product # unused
     @product = Product.new(params[:product])
     if @product.save
       flash[:notice] = 'Product was successfully created.'
@@ -213,11 +252,11 @@ class AdminController < ApplicationController
     end
   end
 
-  def edit_product
+  def edit_product # unused
     @product = Product.find(params[:id])
   end
 
-  def update_product
+  def update_product # unused
     @product = Product.find(params[:id])
     if @product.update_attributes(params[:product])
       flash[:notice] = 'Product was successfully updated.'
@@ -227,7 +266,7 @@ class AdminController < ApplicationController
     end
   end
 
-  def destroy_product
+  def destroy_product # unused
     Product.find(params[:id]).destroy
     redirect_to :action => 'list'
   end
@@ -251,7 +290,7 @@ class AdminController < ApplicationController
     end
   end
 
-  def add_coupons
+  def add_coupons # unused
     if params[:form]
       form = params[:form]
       lines = form[:coupons].split("\r\n")
@@ -268,8 +307,7 @@ class AdminController < ApplicationController
     end
   end
 
-  # Mass order
-  def mass_order
+  def mass_order # unused
     if params[:form]
       form = params[:form]
       for key in form.keys()
