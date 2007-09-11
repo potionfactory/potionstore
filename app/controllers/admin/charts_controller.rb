b1bb08bb1ec6df6654675ae0b36313f55d06097b
andykim78 <andykim78@25f2d1f3-9e33-0410-bf15-d1b47774bbb0>
2007-09-11 05:33:37 +0000

    Improved admin reports
    git-svn-id: https://potionstore.googlecode.com/svn/trunk@36 25f2d1f3-9e33-0410-bf15-d1b47774bbb0

--- app/controllers/admin/charts_controller.rb
+++ app/controllers/admin/charts_controller.rb
@@ -2,41 +2,44 @@ class Admin::ChartsController < ApplicationController
 
   before_filter :check_authentication
 
-  def last_30_days
-    query_results = Order.find_by_sql(
-        "select date_part('year', orders.order_time) as year,
-                date_part('month', orders.order_time) as month,
-                date_part('day', orders.order_time) as day,
-                extract('day' from age(orders.order_time)) + 1 as days_ago,
-                sum(line_items.unit_price * quantity) - sum(coalesce(coupons.amount, 0))
-                    - sum(line_items.unit_price * quantity * coalesce(percentage, 0) / 100) as revenue,
+  def revenue_history_days
+    # NOTE: mysql uses year, month, and day functions instead of date_part
+    limit = 30
+    query_results = Order.connection.select_all(
+        "select extract(year from orders.order_time) as year,
+                extract(month from orders.order_time) as month,
+                extract(day from orders.order_time) as day,
+                extract(day from age(date_trunc('day', orders.order_time))) as days_ago,
+                sum(line_items.unit_price * quantity)
+                  - sum(coalesce(coupons.amount, 0))
+                  - sum(line_items.unit_price * quantity * coalesce(percentage, 0) / 100) as revenue,
                 max(orders.order_time) as last_time
 
          from orders
               inner join line_items on orders.id = line_items.order_id
               left outer join coupons on coupons.id = orders.coupon_id
 
-         where status = 'C' and lower(payment_type) != 'free' and current_date - 30 <= order_time
+         where status = 'C' and lower(payment_type) != 'free' and current_date - #{limit-1} <= order_time
 
          group by year, month, day, days_ago
 
-         order by last_time desc limit 30")
+         order by last_time desc")
 
     labels = []
     data = []
     
-    0.upto(29) {
-      labels += ['']
-      data += [0]
+    0.upto(limit-1) {
+      labels << ''
+      data << 0
     }
 
-    query_results.each {|day|
-      xindex = -day.days_ago.to_i + 29
-      next if xindex < 0 || xindex > 29
-      revenue = day.revenue.to_f.round
+    query_results.each {|x|
+      xindex = -x['days_ago'].to_i + limit-1
+      next if xindex < 0 || xindex > limit-1
+      revenue = x['revenue'].to_f.round
 
-      d = Date.strptime("#{day.month}/#{day.day}", '%m/%d')
-      labels[xindex] = d.wday == 0 ? "#{day.month}/#{day.day}" : ""
+      d = Date.strptime("#{x['month']}/#{x['day']}", '%m/%d')
+      labels[xindex] = d.wday == 0 ? "#{x['month']}/#{x['day']}" : ""
       data[xindex] = revenue
     }
     
@@ -45,13 +48,60 @@ class Admin::ChartsController < ApplicationController
     render :text => g.render
   end
 
-  def last_12_months
+  def revenue_history_weeks
+    limit = 26
+    query_results = Order.connection.select_all(
+       "select extract(week from orders.order_time) as week,
+               sum(line_items.unit_price * quantity)
+                 - sum(coalesce(coupons.amount, 0))
+                 - sum(line_items.unit_price * quantity * coalesce(percentage, 0) / 100) as revenue,
+                max(orders.order_time) as last_time
+
+          from orders
+               inner join line_items on orders.id = line_items.order_id
+               left outer join coupons on coupons.id = orders.coupon_id
+
+         where status = 'C' and lower(payment_type) != 'free' and current_date - #{(limit-1)*7} <= order_time
+
+         group by week
+
+         order by last_time desc limit #{limit}")
+
+    labels = []
+    data = []
+    
+    0.upto(limit-1) {
+      labels << ''
+      data << 0
+    }
+
+    today = Date.today
+    
+    query_results.each {|x|
+      weeks_ago = today.cweek - x['week'].to_i
+      xindex = -weeks_ago + limit-1
+      next if xindex < 0 || xindex > limit-1
+      revenue = x['revenue'].to_f.round
+
+      labels[xindex] = x['week']
+      data[xindex] = revenue
+    }
+    
+    g = new_chart(data, labels)
+    g.set_x_label_style(10, '#000000', 0, 2)
+    
+    render :text => g.render
+  end
+
+  def revenue_history_months
+    limit = 12
     query_results = Order.find_by_sql(
-        "select date_part('year', orders.order_time) as year,
-                date_part('month', orders.order_time) as month,
-                extract('month' from age(date_trunc('month', orders.order_time))) as months_ago,
-                sum(line_items.unit_price * quantity) - sum(coalesce(coupons.amount, 0))
-                    - sum(line_items.unit_price * quantity * coalesce(percentage, 0) / 100) as revenue,
+        "select extract(year from orders.order_time) as year,
+                extract(month from orders.order_time) as month,
+                extract(month from age(date_trunc('month', orders.order_time))) as months_ago,
+                sum(line_items.unit_price * quantity)
+                  - sum(coalesce(coupons.amount, 0))
+                  - sum(line_items.unit_price * quantity * coalesce(percentage, 0) / 100) as revenue,
                 max(orders.order_time) as last_time
 
          from orders
@@ -62,23 +112,23 @@ class Admin::ChartsController < ApplicationController
 
          group by year, month, months_ago
 
-         order by last_time desc limit 12")
+         order by last_time desc limit #{limit}")
 
     labels = []
     data = []
     
-    0.upto(11) {
-      labels += ['']
-      data += [0]
+    0.upto(limit-1) {
+      labels << ''
+      data << 0
     }
 
-    query_results.each {|month|
-      xindex = -month.months_ago.to_i + 11
-      next if xindex < 0 || xindex > 11
-      revenue = month.revenue.to_f.round
+    query_results.each {|x|
+      xindex = -x['months_ago'].to_i + limit-1
+      next if xindex < 0 || xindex > limit-1
+      revenue = x['revenue'].to_f.round
 
-      d = Date.strptime("#{month.month}/#{month.year}", '%m/%y')
-      labels[xindex] = "#{month.month}/#{month.year}"
+      d = Date.strptime("#{x['month']}/#{x['year']}", '%m/%y')
+      labels[xindex] = "#{x['month']}/#{x['year']}"
       data[xindex] = revenue
     }
     
@@ -93,14 +143,14 @@ class Admin::ChartsController < ApplicationController
     g = OpenFlashChart.new
 
     g.set_data(data)
-    g.bar_filled(75, '#000000', '#333333', ' ', 10)
+    g.bar_filled(75, '#000000', '#333333', 'revenue', 10)
     g.set_bg_color('#FFFFFF')
 
     g.set_x_labels(labels)
     g.set_x_label_style(10, '#000000', 0, 1)
     g.set_x_axis_steps(0)
 
-    g.set_y_max((data.max/100).ceil * 100)
+    g.set_y_max((data.max.to_f/100).ceil * 100)
     g.set_y_label_steps(5)
 
     g.instance_variable_set :@y_axis_color, '#AAAAAA'
