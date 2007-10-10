b311512ff9309fd93d440639d837be4b675d6b73
Andy Kim <andy@potionfactory.com>
2007-10-10 16:09:57 -0700

    Optional form fields are labeled with a value

--- public/javascripts/store.js
+++ public/javascripts/store.js
@@ -28,6 +28,25 @@ function correctPNG() // correctly handle PNG transparency in Win IE 5.5 & 6.
    }    
 }
 
+function setup_help_values() {
+	for (element_id in HELP_VALUES) {
+		setup_help_value(element_id);
+	}
+}
+
+function setup_help_value(element_id) {
+	var element = $(element_id).get(0);
+	var help_text = HELP_VALUES[element_id];
+	if (element.value == help_text) {
+		element.value = '';
+		element.style.color = '#000';
+	}
+	else if (element.value == '') {
+		element.style.color = '#aaa';
+		element.value = help_text;
+	}
+}
+
 /*
  * jQuery 1.1.4 - New Wave Javascript
  *
