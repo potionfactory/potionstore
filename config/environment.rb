3d2fa42fe63f508ef6b8854bec1791f8f66220cc
Andy Kim <andy@potionfactory.com>
2008-01-13 08:40:47 -0800

    Put back in the session key configuration

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,7 @@ Rails::Initializer.run do |config|
   # Use the database for sessions instead of the file system
   # (create the session table with 'rake db:sessions:create')
   # config.action_controller.session_store = :active_record_store
+  config.action_controller.session = { :session_key => "_potion_store_session", :secret => "sdaf0022s94hfdbz32sdjfhf4j-=123sdh" }
   
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper, 
