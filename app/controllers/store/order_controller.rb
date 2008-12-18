bbff3ac9e028d0f6421c3da51eef9af0365d93e2
Andy Kim <andy@potionfactory.com>
2008-12-17 18:18:50 -0800

    Using .blank? more

--- app/controllers/store/order_controller.rb
+++ app/controllers/store/order_controller.rb
@@ -43,7 +43,7 @@ class Store::OrderController < ApplicationController
     coupon_text = params[:coupon].strip
     @order.coupon_text = coupon_text
 
-    if coupon_text != '' && @order.coupon == nil
+    if !coupon_text.blank? && @order.coupon == nil
       coupon = Coupon.find_by_coupon(coupon_text)
       if coupon != nil && coupon.expired?
         flash[:notice] = 'Coupon Expired'
