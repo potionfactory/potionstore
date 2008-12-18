929118b492e27add3b8466146243826f22841538
Andy Kim <andy@potionfactory.com>
2008-12-17 16:26:13 -0800

    Refactored out country mapping into a YML file and fixed some url helper calls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
+def load_country_names()
+  app_root = File.dirname(__FILE__) + '/../..'
+  config_dir = app_root + '/config/'
+  ymlpath = File.expand_path(config_dir + 'countries.yml')
+  $COUNTRY_NAMES = YAML.load(File.open(ymlpath))
+end
+
+load_country_names()
+
 module ApplicationHelper
+  def country_name(country_code)
+    return $COUNTRY_NAMES[country_code]
+  end
 end
