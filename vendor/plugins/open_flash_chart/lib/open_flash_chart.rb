782664f6b3aba25d38e88b8515b213f54f36531d
Matthieu Cormier <mcormier@cs.dal.ca>
2009-05-18 15:25:38 -0300

    Issue 22 fix:Absolute URLS for images/stylesheets

--- vendor/plugins/open_flash_chart/lib/open_flash_chart.rb
+++ vendor/plugins/open_flash_chart/lib/open_flash_chart.rb
@@ -293,8 +293,8 @@ class OpenFlashChart
     url     = CGI::escape(url)
     output  = "<object classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" width=\"#{width}\" height=\"#{height}\" id=\"graph\" align=\"middle\">"
     output += '<param name="allowScriptAccess" value="sameDomain" />'
-    output += "<param name=\"movie\" value=\"/open-flash-chart.swf?width=#{width.to_s} &height=#{height.to_s} &data=#{url}\" /><param name=\"quality\" value=\"high\" /><param name=\"bgcolor\" value=\"#FFFFFF\" />"
-    output += '<embed src="/open-flash-chart.swf?width=' + width.to_s + '&height=' + height.to_s + '&data=' + url.to_s + '" quality="high" bgcolor="#FFFFFF" width="' +  width.to_s + '" height="' + height.to_s + '" name="open-flash-chart" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />'
+    output += "<param name=\"movie\" value=\"" + ActionController::Base.relative_url_root + "/open-flash-chart.swf?width=#{width.to_s} &height=#{height.to_s} &data=#{url}\" /><param name=\"quality\" value=\"high\" /><param name=\"bgcolor\" value=\"#FFFFFF\" />"
+    output += '<embed src="' + ActionController::Base.relative_url_root + '/open-flash-chart.swf?width=' + width.to_s + '&height=' + height.to_s + '&data=' + url.to_s + '" quality="high" bgcolor="#FFFFFF" width="' +  width.to_s + '" height="' + height.to_s + '" name="open-flash-chart" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />'
     output += '</object>'
 		return output
   end
