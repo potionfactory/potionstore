0390f3691c06f4a56f224a8b2f1e8de37882f28b
Andy Kim <andy@potionfactory.com>
2011-01-07 19:59:54 -0800

    Fixed some test cases

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -4,7 +4,7 @@ require 'admin_controller'
 # Re-raise errors caught by the controller.
 class AdminController; def rescue_action(e) raise e end; end
 
-class AdminControllerTest < Test::Unit::TestCase
+class AdminControllerTest < ActiveSupport::TestCase
   fixtures :products
 
   def setup
