7f7ab500809a94905f9f68e47c069f9be0a257b0
Andy Kim <andy@potionfactory.com>
2011-09-03 15:03:10 -0700

    Merged two migration files into one to speed up migration if you have lots of orders

--- db/migrate/20110112001916_add_total_and_unique_id_to_orders.rb
+++ db/migrate/20110112001916_add_total_and_unique_id_to_orders.rb
@@ -2,22 +2,26 @@ def round_money(amount)
   return ("%01.2f" % amount).to_f()
 end
 
-class AddTotalToOrders < ActiveRecord::Migration
+class AddTotalAndUniqueIdToOrders < ActiveRecord::Migration
   def self.up
+    add_column :orders, :unique_id, :string, :limit => 16
     add_column :orders, :total, :decimal, :precision => 10, :scale => 2
 
     Order.reset_column_information
 
     Order.find(:all).each do |o|
+      o.unique_id = uid()
       o.total = o.calculated_total
       o.skip_cc_validation = true
       o.save!
     end
 
+    change_column :orders, :unique_id, :string, :limit => 16, :null => false
     change_column :orders, :total, :decimal, :precision => 10, :scale => 2, :null => false
   end
 
   def self.down
     remove_column :orders, :total
+    remove_column :orders, :unique_id
   end
 end
