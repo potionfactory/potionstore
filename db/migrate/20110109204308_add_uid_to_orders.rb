3331b5e6d0c823f4c462cc2a2236a498aded4ece
Andy Kim <andy@potionfactory.com>
2011-11-22 13:28:16 -0800

    Fixed a Potion Factory custom class reference having seeped into the master branch. Renamed uuid column to unique_id.

--- db/migrate/20110109204308_add_uid_to_orders.rb
+++ db/migrate/20110109204308_add_uid_to_orders.rb
@@ -3,19 +3,19 @@ require 'uid'
 
 class AddUidToOrders < ActiveRecord::Migration
   def self.up
-    add_column :orders, :uuid, :string, :limit => 16
+    add_column :orders, :unique_id, :string, :limit => 16
     Order.reset_column_information
 
     Order.find(:all).each do |o|
-      o.uuid = uid()
+      o.unique_id= uid()
       o.skip_cc_validation = true
       o.save!
     end
 
-    change_column :orders, :uuid, :string, :limit => 16, :null => false
+    change_column :orders, :unique_id, :string, :limit => 16, :null => false
   end
 
   def self.down
-    remove_column :orders, :uuid
+    remove_column :orders, :unique_id
   end
 end
