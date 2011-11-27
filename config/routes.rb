e9a9bcb089cbecccd04895073b2ba63890cc0ec8
Ben Mills <ben@unfiniti.com>
2011-11-26 23:19:40 -0700

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,17 @@ Potionstore::Application.routes.draw do
   match 'store' => 'store/order#index'
   match '' => 'store/order#index'
   resource :order
-  resources :products
-  resources :orders do
   
-    member do
-  get :cancel
-  get :uncancel
-  get :refund
-  get :send_emails
-  end
-  
+  namespace :admin do
+    resources :products
+    resources :orders do
+      member do
+        get :cancel
+        get :uncancel
+        get :refund
+        get :send_emails
+      end
+    end
   end
 
   match 'admin/charts/:action' => 'admin/charts#index'
