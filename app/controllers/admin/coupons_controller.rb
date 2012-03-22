5a6233802ffee0489983b9e374ca0a296cdf42ce
Ben Mills <ben@unfiniti.com>
2012-03-21 19:40:12 -0600

    Use more strict SQL so postgresql is happy

--- app/controllers/admin/coupons_controller.rb
+++ app/controllers/admin/coupons_controller.rb
@@ -3,7 +3,7 @@ class Admin::CouponsController < ApplicationController
   before_filter :check_authentication
 
   def index
-    @coupons = Coupon.find_by_sql("select count(*) as count, i.code, i.product_code from coupons i group by i.code order by i.product_code")
+    @coupons = Coupon.find_by_sql("select count(*) as count, i.code, i.product_code from coupons i group by i.code order by i.product_code,i.code,count")
   end
 
   def show
